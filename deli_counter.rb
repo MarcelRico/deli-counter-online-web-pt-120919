ea162aee684b04b4af1d12752196a812fd3b4b6e
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 02:57:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def line(queue)
+  if(queue.length == 0)
+    puts "The line is currently empty."
+  else
+    results = ["The line is currently:"]
+  
+    queue.length.times do |idx|
+      results << " #{idx+1}. #{queue[idx]}"
+    end
+    
+    puts results.join()
+  end
+end
+
+def take_a_number(queue,name)
+  
+  phrase = ""
+  if(queue.length == 0)
+    phrase = "Welcome, #{name}. You are number 1 in line."
+  else
+    phrase = "Welcome, #{name}. You are number #{queue.length} in line."
+  end
+  
+  puts phrase
+  
+  queue << name
+end
